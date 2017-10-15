92d7788099b583af99ebc2a91ba08cec0955a1c8
ahm388 <ahm388@gmail.com>
2017-10-15 22:31:43 +0500

    Configured Css and Js and Set up database and mail

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ahm388@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Ali Hassan'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('ahm388'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('naichsahab'),
 
     /*
     |--------------------------------------------------------------------------
